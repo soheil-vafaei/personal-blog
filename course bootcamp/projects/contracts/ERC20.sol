02b65e9ae7b80811465a3b5c9038a48d9c092f52
soheil-vafaei <soheil.vafaei.official@gmail.com>
2023-08-08 18:37:54 +0330

    add Contracts

---
+++ course bootcamp/projects/contracts/ERC20.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// import "./IERC721.sol";
+
+contract ERC721 {
+
+            // mappings //
+
+    // mapping _balances
+    // mapping _allowances
+
+
+            // variables //
+
+    // uint256 _totalSupply
+    // string _name
+    // string _symbol
+
+    //constructor (_name , _symbol)
+
+
+            // metadata fucntionws //
+    // function name
+    // function symbol
+    // function decimals
+
+
+            // override functions // 
+
+    // function totalSupply
+    // function balanceOf
+    // function transfer
+    // fucntion allowance
+    // function approve
+    // function trasnferFrom
+
+
+            // overloading functions // 
+
+    // function _transfer
+    // function _mint
+    // function _approve
+    // function _spendAllowance
+    
+}
